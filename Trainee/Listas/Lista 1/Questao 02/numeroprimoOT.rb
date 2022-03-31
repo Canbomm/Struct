874351c695e121df21fd00b2ff42b71ae8e881f0
Canbomm <canbomim@hotmail.com>
2022-03-30 21:21:12 -0300

    0.2.2

---
+++ Trainee/Listas/Lista 1/Questao 02/numeroprimoOT.rb
@@ -0,0 +1,23 @@
+numero = gets.to_i
+
+def eprimo(a)
+    for x in (2...a).to_a
+        if a % x == 0
+            return false
+        end
+    end
+    return true
+end
+
+if numero < 2
+    puts"Não foi possível encontrar um número primo"
+else
+    while numero >= 2
+        if eprimo(numero) == true
+            maiorprimo = numero
+            break
+        end
+        numero -= 1
+    end
+    p maiorprimo
+end
